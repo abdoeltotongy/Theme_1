46e8c90592186242fd9bf8f8de623b6b4236ff45
abdoeltotongy <abdoeltotongy2@gmail.com>
2021-08-02 21:09:22 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,116 @@
+jQuery(function ($) {
+    'use strict';
+
+    //#main-slider
+    $(function () {
+        $('#main-slider.carousel').carousel({
+            interval: 8000
+        });
+    });
+
+    $('.window-height').height($(window).height());
+
+    // accordian
+    $('.accordion-toggle').on('click', function () {
+        $(this).closest('.panel-group').children().each(function () {
+            $(this).find('>.panel-heading').removeClass('active');
+        });
+
+        $(this).closest('.panel-heading').toggleClass('active');
+    });
+
+/*============= Cart Toggler ==============*/
+
+
+
+    function cartToggler() {
+        var trigger = $('.visible-minicart'),
+          container = $('.minicart__active');
+        trigger.on('click', function (e) {
+          e.preventDefault();
+          container.toggleClass('is-visible');
+
+        });
+        trigger.on('click', function (e) {
+          e.preventDefault();
+          container.toggleClass('');
+
+        });
+        $('.micart__close').on('click', function () {
+          container.removeClass('is-visible');
+        });
+    }
+    cartToggler();
+
+        $(".visible-minicart").on("click" , function(){
+        $(".block-minicart.is-visible").css("visibility", "visible");
+    });
+
+    
+/*============= Setting Toggler ==============*/
+    // portfolio filter
+    $(window).load(function () {
+        'use strict';
+        var $portfolio_selectors = $('.portfolio-filter >li>a');
+        var $portfolio = $('.portfolio-items');
+        $portfolio.isotope({
+            itemSelector: '.portfolio-item',
+            layoutMode: 'fitRows'
+
+        });
+
+        $portfolio_selectors.on('click', function () {
+            $portfolio_selectors.removeClass('active');
+            $(this).addClass('active');
+            var selector = $(this).attr('data-filter');
+            $portfolio.isotope({
+                filter: selector
+            });
+            return false;
+        });
+    });
+
+    // Contact form
+    var form = $('#main-contact-form');
+    form.submit(function (event) {
+        event.preventDefault();
+        var form_status = $('<div class="form_status"></div>');
+        $.ajax({
+            url: $(this).attr('action'),
+
+            beforeSend: function () {
+                form.prepend(form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Email is sending...</p>').fadeIn());
+            }
+        }).done(function (data) {
+            form_status.html('<p class="text-success">' + data.message + '</p>').delay(3000).fadeOut();
+        });
+    });
+
+    $('.testimonial-slider').owlCarousel({
+        margin: 30,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            }
+        }
+    });
+
+    //goto top
+    $('.gototop').click(function (event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("body").offset().top
+        }, 500);
+    });
+
+    //Pretty Photo
+    $("a[rel^='prettyPhoto']").prettyPhoto({
+        social_tools: false
+    });
+});
